b35593b7ba0990421256c1b9c9cae77c5a276b49
saullo.reis <saulloreis01@hotmail.com>
2024-07-13 10:58:57 -0300

    adding authMiddleware to handlers for admins

--- src/handlers/user/auth.go
+++ src/handlers/user/auth.go
@@ -42,3 +42,46 @@ func AuthMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func AuthMiddlewareAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		fmt.Println(authHeader)
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error_code": 401,
+				"message":    "User unauthorized",
+			})
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.TrimSpace(strings.Replace(authHeader, "Bearer", "", 1))
+		claims := &jwt.RegisteredClaims{}
+
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error_code": 401,
+				"message":    "Invalid token",
+			})
+			c.Abort()
+			return
+		}
+
+		if claims.Subject != "admin" {
+			c.JSON(http.StatusForbidden, gin.H{
+				"error_code": 403,
+				"message":    "Access forbidden: only admin can access",
+			})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Subject)
+		c.Next()
+	}
+}
